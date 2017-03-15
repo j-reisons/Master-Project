f59b4da3740fe17fe0c57ca6ba74bbe32fd48d7d
j-reisons <j.reisons@gmail.com>
2017-03-15 14:18:06 +0100

    run_battery_iter function for further testing of TDVP

--- Models/Heisenberg_Batteries/run_battery_iter.m
+++ Models/Heisenberg_Batteries/run_battery_iter.m
@@ -1,20 +1,9 @@
-clc
-close all
-clear all
-%%
-% I run Heisenberg Batteries model for known parameters and compare
-% currents and magnetization results between TEBD and TDVP
-
-N = 10;
-Uc = 0.5;
-Ub = 0;
-tag = 'TDVP';
+function run_battery_iter(N,U_c,U_b,D_max,tag)
 
 J = 1;
 d = 2;
 
-D_start = 100;
-D_max = 200;
+D_start = D_max;
 
 T = N/(2*J);
 dt = 0.05/J;
@@ -22,12 +11,11 @@ steps = round(T/dt);
 
 ground_error = 1E-8;
 
-filename = ['Batteries','_N',strrep(num2str(N),'.',',') ,'_Ub',strrep(num2str(Ub),'.',',')...
-    ,'_Uc',strrep(num2str(Uc),'.',','),'_','T',strrep(num2str(T),'.',','),'_','Dmax',num2str(D_max)...
+filename = ['Batteries','_N',strrep(num2str(N),'.',',') ,'_Ub',strrep(num2str(U_b),'.',',')...
+    ,'_Uc',strrep(num2str(U_c),'.',','),'_','T',strrep(num2str(T),'.',','),'_','Dmax',num2str(D_max)...
     ,'_',tag,'.mat'];
 
 %% Initial state preparation
-
 State = cell(1,3*N);
 
 Up = zeros(1,1,2);
@@ -35,7 +23,7 @@ Up(1,1,1) = 1;
 Down = zeros(1,1,2);
 Down(1,1,2) = 1;
 
-H_Heis = Heisenberg_H(N,J,Uc);
+H_Heis = Heisenberg_H(N,J,U_c);
 MPS = random_mps(N,D_start,d,-1);
 Ground = Iter_ground(H_Heis,MPS,ground_error);
 
@@ -46,10 +34,11 @@ for i=1:N
 end
 
 %%
-Hamiltonian = Batteries_H(N,J,Uc,Ub);
+Hamiltonian = Batteries_H(N,J,U_c,U_b);
 
 Magnetizations = zeros(3*N,steps+1);
 Currents = zeros(3*N - 1,steps+1);
+
 
 S_Z = [1,0;0,-1];
 S_X =[0,1;1,0];
@@ -73,9 +62,9 @@ Q_mpo{2} = Q_2;
 State = Inflate_mps(State,D_max);
 State = sweep(State,-1);
 canon = -1;
-%%
 
 for i = 1:steps
+    i
     evaluations = Canon_evaluator(State,canon,S_Z_mpo,Q_mpo);
     Magnetizations(:,i) = real(evaluations{1});
     Currents(:,i) = real(evaluations{2});
@@ -87,16 +76,6 @@ evaluations = Canon_evaluator(State,canon,S_Z_mpo,Q_mpo);
 Magnetizations(:,steps+1) = real(evaluations{1});
 Currents(:,steps+1) = real(evaluations{2});
 
-%%
+
 save(filename,'Magnetizations','Currents','State')
-
-%%
-load('Batteries_N10_Ub0_Uc0,5_T5_Dmax100_TDVP.mat');
-Magnetizations_TDVP = Magnetizations;
-Currents_TDVP = Currents;
-load('Batteries_N10_Ub0_Uc0,5_T5_Dmax100_threeU.mat')
-
-%%
-imagesc(Currents-Currents_TDVP)
-figure
-imagesc(Magnetizations-Magnetizations_TDVP)
+end
